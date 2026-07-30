f1ee70a42d3be513a35e40703df589da5864e488
crmsgomes-hue <crmsgomes@gmail.com>
2026-07-30 10:11:19 +0100

    Notificacao persistente + abre direto no painel

--- push/sw-push.js
+++ push/sw-push.js
@@ -5,7 +5,11 @@ self.addEventListener('push', function(event) {
     self.registration.showNotification(data.title || 'CoreJoint', {
       body: data.body || '',
       icon: 'icon-192.png',
-      badge: 'icon-192.png'
+      badge: 'icon-192.png',
+      requireInteraction: true,
+      vibrate: [200, 100, 200, 100, 200],
+      tag: 'cj-pendencias',
+      renotify: true
     })
   );
 });
@@ -13,6 +17,6 @@ self.addEventListener('push', function(event) {
 self.addEventListener('notificationclick', function(event) {
   event.notification.close();
   event.waitUntil(
-    clients.openWindow('https://crmsgomes-hue.github.io/BAGGA1/CoreJoint_Reportes.html')
+    clients.openWindow('https://crmsgomes-hue.github.io/BAGGA1/CoreJoint_Reportes.html?gestao=1')
   );
 });
